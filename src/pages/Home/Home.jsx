d6addd153be92b2d37d23a6e4f74472454297f8d
Habeeb Muhammad P <habeebmuhammadp@gmail.com>
2024-08-20 12:40:47 +0530

    Add Play and Info Button on functional

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,14 @@ import Navbar from '../../components/Navbar/Navbar';
 import TitleCards from '../../components/TitleCards/TitleCards';
 import axios from 'axios';
 import { imageUrl } from '../../components/const';
+import Player from '../Player/Player';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 
 function Home() {
   const [movie, setMovie] = useState(null);
+  const Navigate=useNavigate()
 
   useEffect(() => {
     axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=2cacca13492936e7caaa8a33930f5aff&language=en-US`)
@@ -15,6 +20,14 @@ function Home() {
         setMovie(response.data.results[0]);
       });
   }, []);
+  const handlePlayClick=()=>{
+    if(movie){
+      Navigate(`/Player/${movie.id}`)
+    }
+  }
+const info=()=>{
+    toast.info(`${movie.overview}`)
+  }
 
   return (
     <div className="home">
@@ -26,8 +39,8 @@ function Home() {
         <div className="content">
           <h1 className="title">{movie ? movie.title || movie.name : ""}</h1>
           <div className="Banner_Buttons">
-            <button className="button"><img src='play.png'/>Play</button>
-            <button className="button"><img src='info.jpg'/>Info</button>
+            <button className="button" onClick={handlePlayClick}><img src='play.png'/>Play</button>
+            <button className="button" onClick={info}><img src='info.jpg'/>Info</button>
           </div>
           <h1 className="description">{movie ? movie.overview : ""}</h1>
         </div>
